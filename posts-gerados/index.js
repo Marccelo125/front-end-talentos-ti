aeec2a4ff180ec0a09ce0d17f483058f1c5ecbe5
Marcelo <contato.marccelo125@gmail.com>
2023-11-16 11:11:59 -0300

    :recycle: refactor: html & JS automatizados

--- posts-gerados/index.js
+++ posts-gerados/index.js
@@ -1,32 +1,5 @@
 const centroDePosts = document.querySelector(".posts");
 
-let dadosDoPost = [
-  {
-    titulo: "Titulo para o post - 01",
-    resumo: "Resumo deste post acima",
-    imagem: "https://picsum.photos/300/200",
-    link: "./posts/20231017-primeiro-post.html"
-},
-{
-    titulo: "Titulo para o post - 02",
-    resumo: "Resumo deste post acima",
-    imagem: "https://picsum.photos/seed/picsum/300/200",
-    link: "./posts/20231017-segundo-post.html"
-},
-{
-    titulo: "Titulo para o post - 03",
-    resumo: "Resumo deste post acima",
-    imagem: "https://picsum.photos/300/200?grayscale",
-    link: "./posts/20231018-rascunho-para-amanha.html"
-  },
-  {
-      titulo: "Titulo para o post - 04",
-      resumo: "Resumo deste post acima",
-      imagem: "https://picsum.photos/300/200/?blur",
-      link: "./posts/20231018-tarefa02.html"
-    },
-];
-
 // let novoDadosPost = confirm("Você deseja adicionar um post?");
 
 // while (novoDadosPost) {
@@ -39,32 +12,42 @@ let dadosDoPost = [
 //   }
 // }
 
-const numPosts = dadosDoPost.length;
+async function leposts() {
+  //Lê dados do servidor
+  const resposta = await fetch("posts.json");
+  const dadosDoPost = await resposta.json();
 
-for (let i = 0; i < numPosts; i++) {
-  const novoPost = document.createElement("article");
-  const verArtigo = document.createElement("p");
+  for (post of dadosDoPost) {
+    const novoPost = document.createElement("article");
+    const verArtigo = document.createElement("p");
 
-  novoPost.innerHTML = `
-  <picture><img src="${dadosDoPost[i].imagem}"/></picture>
-    <header>${dadosDoPost[i].titulo}
-        <details>
-            <summary>veja um resumo...</summary>
-            <p>${dadosDoPost[i].resumo}</p>
-        </details>
-    </header>
-    `;
+    novoPost.innerHTML = `
+  <picture><img src="${post.imagem}"/></picture>
+  <header><p id="titulo-post">${post.titulo}</p>
+    <time datetime="${post.data}" class="data-horario">
+      ${post.data} - ${post.horario}
+    </time>
+    <p><strong>${post.autor}</strong></p>
+    <details>
+      <summary>veja um resumo...</summary>
+      <p>${post.resumo}</p>
+    </details>
+      </header>
+      `;
 
-  verArtigo.innerHTML = `    
+    verArtigo.innerHTML = `    
     <p>
-    <a href="${dadosDoPost[i].link}"
+    <a href="${post.link}"
     >Ver artigo Completo</a
     >
     </p>
     `;
 
-  novoPost.classList.add("posts");
-  verArtigo.classList.add("botao-artigo");
-  novoPost.appendChild(verArtigo);
-  centroDePosts.appendChild(novoPost);
+    novoPost.classList.add("posts");
+    verArtigo.classList.add("botao-artigo");
+    novoPost.appendChild(verArtigo);
+    centroDePosts.appendChild(novoPost);
+  }
 }
+
+leposts();
